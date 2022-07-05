da4690015cacb556cc3cf6cc736ba6aec6fbd1e0
Veronika <vvnika@mail.ru>
2022-07-05 22:43:35 +1100

    add "Задача 4"

--- Program.cs
+++ Program.cs
@@ -99,6 +99,7 @@ else
 */
 //Домашнее задание
 
+/*
 // Задача 2: Напишите программу, 
 // которая на вход принимает два числа и выдаёт, 
 // какое число большее, а какое меньшее.
@@ -119,4 +120,22 @@ Console.Write("max= ");
 Console.WriteLine (max);
 Console.Write("min= ");
 Console.WriteLine (min);
+*/
 
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+int a = 2;
+int b = 3;
+int c = 7;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max= ");
+Console.WriteLine(max);
