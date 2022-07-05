0f0af6af248953c672ae32ac9d79325a2a17e2c5
Veronika <vvnika@mail.ru>
2022-07-05 22:38:27 +1100

    add "Задача 2_Решение_2_оператор if"

--- Program.cs
+++ Program.cs
@@ -103,17 +103,20 @@ else
 // которая на вход принимает два числа и выдаёт, 
 // какое число большее, а какое меньшее.
 
-Console.WriteLine("Введите число:");
+
 int a=5;
 int b=7;
 
-if (a > b)
-{
-    Console.WriteLine("Да, первое число, больше второго");
-}
-else
-{
-    Console.WriteLine("Нет, первое число меньше второго");
-}
+int max=a;
+int min=b;
 
+if (a>max) max=a;
+if (b>max) max=b;
+if (b<max) min=b;
+if (a<max) min=a;
+
+Console.Write("max= ");
+Console.WriteLine (max);
+Console.Write("min= ");
+Console.WriteLine (min);
 
